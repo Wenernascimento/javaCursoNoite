b487a256ca8395d100299c4d27bef59d90ef53af
Wener Nascimento <wenerfelice@gmail.com>
2024-10-17 18:00:54 -0300

    JavaStream

---
+++ javaCursoNoite/teste/javaCursoNoite/Nomes.java
@@ -0,0 +1,17 @@
+package javaCursoNoite;
+import java.util.Scanner;
+public class Nomes {
+
+	public static void main(String[] args) {
+	Scanner sc = new  Scanner (System.in);
+	String nomenextLine;
+	
+	
+	System.out.print("Digite o Nome:");
+	
+	
+	
+}
+	}
+
+

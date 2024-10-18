6c9caddca2e98e2c3db5da52c7d381b10ee65710
Wener Nascimento <wenerfelice@gmail.com>
2024-10-18 12:48:39 -0300

    InterfaceGrafica

--- javaCursoNoite/teste/javaCursoNoite/Nomes.java
+++ javaCursoNoite/teste/javaCursoNoite/Nomes.java
@@ -5,12 +5,14 @@ public class Nomes {
 	public static void main(String[] args) {
 	Scanner sc = new  Scanner (System.in);
 	String nomenextLine;
+	String sobrenomenextLine;
+	int idade;
 	
 	
-	System.out.print("Digite o Nome:");
-	
-	
-	
+	System.out.println("Digite o Nome:");
+	System.out.println("Digite o sobrenome:");
+	System.out.println("Digite a idade:");
+	 
 }
 	}
 
